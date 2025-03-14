cc7bbe2974e2192208d23e43ca8fb1e7f31b42db
sean okeefe <seanokfe@gmail.com>
2025-03-13 21:59:42 -0400

    DS18B20 temperature convert and report working

--- CM4/BSP/Src/BSP_main.c
+++ CM4/BSP/Src/BSP_main.c
@@ -49,8 +49,8 @@ void DS18B20_delayUS(uint32_t delay)
 	}
 
 	//	//Reset the TIMx_CNT register
-//	htim5.Instance->EGR |= TIM_EGR_UG;
-//	Reset CNT
+	//	htim5.Instance->EGR |= TIM_EGR_UG;
+	//	Reset CNT
 	BSP_htim5->Instance->CNT = 0;	//UIFCPY is read only so okay to clear
 
 	while(BSP_htim5->Instance->CNT < delay);
@@ -74,6 +74,15 @@ uint8_t DS18B20_wire1_read(void)
 	return ((WIRE1_THERM_GPIO_Port->IDR>>14)&0x1);
 }
 
+DS18B20_wire1_interface wire1_fptrs =
+{
+		&DS18B20_delayUS,
+		&DS18B20_wire1_setZero,
+		&DS18B20_wire1_setOne,
+		&DS18B20_wire1_read,
+		0
+};
+DS18B20 devices[DS18B20_MAX_DEVICES] = {0};
 
 void BSP_main(BSP_ARGS *argv)
 {
@@ -90,30 +99,25 @@ void BSP_main(BSP_ARGS *argv)
 
 	if (VIRT_UART_Init(&huart0) != VIRT_UART_OK)
 	{
-	  log_err("VIRT_UART_Init UART0 failed.\r\n");
-	  Error_Handler();
+		log_err("VIRT_UART_Init UART0 failed.\r\n");
+		Error_Handler();
 	}
 	/*Need to register callback for message reception by channels*/
 	if(VIRT_UART_RegisterCallback(&huart0, VIRT_UART_RXCPLT_CB_ID, VIRT_UART0_RxCpltCallback) != VIRT_UART_OK)
 	{
-	  log_err("VIRT_UART_Register_Callback() failed.\r\n");
-	  Error_Handler();
+		log_err("VIRT_UART_Register_Callback() failed.\r\n");
+		Error_Handler();
 	}
 
-	DS18B20 dev[DS18B20_MAX_DEVICES] = {0};
-	DS18B20_BSP_FPTRS dev_fptrs =
-	{
-		  &DS18B20_delayUS,
-		  &DS18B20_wire1_setZero,
-		  &DS18B20_wire1_setOne,
-		  &DS18B20_wire1_read
-	};
+
+
 	uint8_t dev_cnt = 0;
 	//Assign device specific function pointers
 	while (1)
 	{
 		int8_t ret = 0;
-
+		uint8_t message_address[16];
+		uint8_t *msg_ptr = message_address;
 		OPENAMP_check_for_message();
 		if (VirtUart0RxMsg)
 		{
@@ -121,11 +125,8 @@ void BSP_main(BSP_ARGS *argv)
 			switch (VirtUart0ChannelBuffRx[0])
 			{
 			case '0' :
-				uint8_t message_address[16];
-				uint8_t *msg_ptr = message_address;
-
-				ret = DS18B20_init(dev, &dev_fptrs, DS18B20_MAX_DEVICES);
-				VIRT_UART_Transmit(&huart0, "Send wire1_init(): ", sizeof("Send wire1_init(): "));
+				ret = DS18B20_init(devices, &wire1_fptrs, DS18B20_MAX_DEVICES);
+				VIRT_UART_Transmit(&huart0, "\nSend wire1_init(): ", sizeof("\nSend wire1_init(): "));
 				intToHexStr(ret, msg_ptr);
 
 				if(ret >= 0)
@@ -141,7 +142,7 @@ void BSP_main(BSP_ARGS *argv)
 						VIRT_UART_Transmit(&huart0, message_address, 2);
 						for(int y=56; y>=0; y-=8)
 						{
-							intToHexStr(dev[x].address>>y, msg_ptr);
+							intToHexStr(devices[x].address>>y, msg_ptr);
 							msg_ptr +=2;
 						}
 						VIRT_UART_Transmit(&huart0, " Address=0x", sizeof("\nAddress=0x"));
@@ -155,10 +156,72 @@ void BSP_main(BSP_ARGS *argv)
 				}
 
 				break;
+
+			case '1':
+				VIRT_UART_Transmit(&huart0, "\nStart Temperature Conversion per device", sizeof("\nStart Temperature Conversion per device"));
+
+				for(int x=0; x< dev_cnt; x++)
+				{
+					ret = DS18B20_convertTemperatureDevice(&devices[x],0);
+					VIRT_UART_Transmit(&huart0, "\nDev 0x", sizeof("\nDev 0x"));
+					intToHexStr(x, msg_ptr);
+					VIRT_UART_Transmit(&huart0, message_address, 2);
+					if(ret != DS18B20_OK)
+					{
+						VIRT_UART_Transmit(&huart0, " Error = 0x", sizeof(" Error = 0x"));
+						msg_ptr = message_address;
+						intToHexStr(ret, msg_ptr);
+						VIRT_UART_Transmit(&huart0, message_address, 2);
+					}
+					else
+					{
+						VIRT_UART_Transmit(&huart0, " Success", sizeof(" Success"));
+					}
+				}
+
+				break;
+
+			case '2':
+				VIRT_UART_Transmit(&huart0, "\nStart Temperature Conversion All", sizeof("\nStart Temperature Conversion All"));
+				ret = DS18B20_convertTemperatureAll(&devices[0]);
+				if(ret != DS18B20_OK)
+				{
+					VIRT_UART_Transmit(&huart0, " Error = 0x", sizeof(" Error = 0x"));
+
+					msg_ptr = message_address;
+					intToHexStr(ret, msg_ptr);
+					VIRT_UART_Transmit(&huart0, message_address, 2);
+				}
+				else
+				{
+					VIRT_UART_Transmit(&huart0, "\nSuccess", sizeof("\nSuccess"));
+				}
+				break;
+
+			case '3':
+				VIRT_UART_Transmit(&huart0, "\nRead Temperatures", sizeof("\nRead Temperatures"));
+				for(int x=0; x<dev_cnt; x++)
+				{
+					DS18B20_TEMPERATURE temperature;
+					ret = DS18B20_getTemperature(&devices[x], &temperature);
+					VIRT_UART_Transmit(&huart0, "\nDev 0x", sizeof("\nDev 0x"));
+
+					intToHexStr(x, msg_ptr);
+					VIRT_UART_Transmit(&huart0, message_address, 2);
+
+					VIRT_UART_Transmit(&huart0, " Temperature = 0x", sizeof(" Temperature = 0x"));
+					msg_ptr = message_address;
+					intToHexStr(temperature.whole, msg_ptr);
+					VIRT_UART_Transmit(&huart0, message_address, 2);
+				}
+				break;
+
 			default:
 				VIRT_UART_Transmit(&huart0, VirtUart0ChannelBuffRx, VirtUart0ChannelRxSize);
 				break;
 			}
+			VIRT_UART_Transmit(&huart0, "\n", sizeof("\n"));
+
 		}
 	}
 }
@@ -176,11 +239,11 @@ void BSP_main(BSP_ARGS *argv)
 void VIRT_UART0_RxCpltCallback(VIRT_UART_HandleTypeDef *huart)
 {
 
-    log_info("Msg received on VIRTUAL UART0 channel:  %s \n\r", (char *) huart->pRxBuffPtr);
+	log_info("Msg received on VIRTUAL UART0 channel:  %s \n\r", (char *) huart->pRxBuffPtr);
 
-    /* copy received msg in a variable to sent it back to master processor in main infinite loop*/
-    VirtUart0ChannelRxSize = huart->RxXferSize < MAX_BUFFER_SIZE? huart->RxXferSize : MAX_BUFFER_SIZE-1;
-    memcpy(VirtUart0ChannelBuffRx, huart->pRxBuffPtr, VirtUart0ChannelRxSize);
-    VirtUart0RxMsg = SET;
+	/* copy received msg in a variable to sent it back to master processor in main infinite loop*/
+	VirtUart0ChannelRxSize = huart->RxXferSize < MAX_BUFFER_SIZE? huart->RxXferSize : MAX_BUFFER_SIZE-1;
+	memcpy(VirtUart0ChannelBuffRx, huart->pRxBuffPtr, VirtUart0ChannelRxSize);
+	VirtUart0RxMsg = SET;
 }
 
